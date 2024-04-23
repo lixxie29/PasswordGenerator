3cc900e5968e0de86ecb2508320d2913ca3323c6
lixxie29 <crina.ciocan@stud.ubbcluj.ro>
2024-04-23 07:47:19 +0300

    7:47am

---
+++ code_crafters/PasswordGenerator/backend/Password.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+
+namespace backend;
+
+public class Password
+{
+    public string value;
+    public bool isValid;
+
+    public Password(string value)
+    {
+        this.value = value;
+        isValid = true;
+    }
+
+    public bool IsValid
+    {
+        get { return isValid; }
+    }
+
+    public bool isPasswordValid(int secLeft)
+    {
+        if (secLeft > 0) return true;
+        return false;
+    }
+
+    public string toString()
+    {
+        return this.value;
+    }
+}
+
